cc454beffb54338d1840c4d62141e9404391fe7f
mathew35 <62357475+mathew35@users.noreply.github.com>
2023-04-19 12:49:29 +0200

    Corrected file names

--- pacman/Sources/main.cpp
+++ pacman/Sources/main.cpp
@@ -1,15 +1,16 @@
 // main.cpp : Defines the entry point for the application.
 //
 
-#include "pacman.h"
+#include "mainwindow.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char* argv[])
 {
 	std::cout << "Hello world in HEEEELLLL!!!!";
 	QApplication a(argc, argv);
+
+
 	mainwindow w;
 	w.show();
-	a.exec();
-	return 0;
+	return a.exec();
 }
---
+++ pacman/Sources/mainwindow.cpp
@@ -0,0 +1,12 @@
+// pacman.cpp : Defines the entry point for the application.
+//
+
+#include "mainwindow.h"
+
+mainwindow::mainwindow(QWidget *parent)
+    : QMainWindow(parent) {
+    ui.setupUi(this);
+}
+
+mainwindow::~mainwindow() {
+}
